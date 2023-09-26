701b8fd57fb42a0c7692ae8828b6ee9a677f3f7b
JCRoooD <juancarlos-99@live.com>
2023-09-26 15:18:27 -0400

    quick fix

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10, 10) # Pick a random number between -10 and 10
+number = random.randint(-10, 10)  # Pick a random number between -10 and 10
 if number > 0:
     print(number, "is positive")
 elif number == 0:
