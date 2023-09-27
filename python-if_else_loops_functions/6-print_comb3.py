2d2d65c293ca015a20e1e9ef5e311d63216cce3e
JCRoooD <juancarlos-99@live.com>
2023-09-27 15:17:24 -0400

    printing all possible combinations

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for first in range(0, 10):
+    for second in range(0, 10):
+        if first == 8 and second == 9:
+            print("{}{}".format(first, second))
+        elif first < second:
+            print("{}{}, ".format(first, second), end="")
+        else:
+            continue
