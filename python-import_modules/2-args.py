555751c998bf09d2fae70d1605f761a599d891ff
JCRoooD <juancarlos-99@live.com>
2023-09-29 16:40:50 -0400

    fixed errors

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 if __name__ == "__main__":
     import sys
 argc = len(sys.argv) - 1
-if argc == 1:
+if argc == 0:
     print("0 arguments.")
-elif argc == 2:
+elif argc == 1:
     print("1 argument:")
-    print("1: {}".format(sys.argv[1]))
+    print("1: {}".format(str(sys.argv[1])))
 else:
     print("{} arguments:".format(argc))
     for i in range(1, argc + 1):
