8ea6ec895ab776a3e681f735e666824f98e8f6ad
JCRoooD <juancarlos-99@live.com>
2023-10-05 13:42:19 -0400

    fixed errors #2

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix is None:
-        return None
-    for i in range(len(matrix)):
-        matrix[i] = list(map(lambda x: x**2, matrix[i]))
-    return matrix
+    new_matrix = []
+    for i in matrix:
+        new_matrix.append([j**2 for j in i])
+    return new_matrix
