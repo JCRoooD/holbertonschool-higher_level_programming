cd49db38b0b611bcc3b5c7f4f37356fa7b8b76f4
JCRoooD <juancarlos-99@live.com>
2023-10-10 15:09:08 -0400

    completing square

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a class Square"""
+
+
+class Square():
+    """This class defines a square"""
+    def __init__(self, size=0):
+        """This method initializes a new instance of Square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self._Square__size = size
