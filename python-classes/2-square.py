9a2ae487629785fddb60a04f899cc2708953b743
JCRoooD <juancarlos-99@live.com>
2023-10-10 15:13:01 -0400

    removed parenthesis

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -2,9 +2,8 @@
 """This module defines a class Square"""
 
 
-class Square():
+class Square:
     """This class defines a square"""
-    
     def __init__(self, size=0):
         """This method initializes a new instance of Square"""
         if type(size) is not int:
