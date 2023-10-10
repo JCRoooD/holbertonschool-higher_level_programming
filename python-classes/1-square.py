118e62070879def98fde55d71ae8240e9c1d3019
JCRoooD <juancarlos-99@live.com>
2023-10-10 14:55:46 -0400

    Private instance attribute: size

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a class Square"""
+
+
+class Square():
+    """This class defines a square"""
+    def __init__(self, size=0):
+        """This method initializes a new instance of Square"""
+        self._Square__size = size
