645ac6fbd9ba32827359dd1f188f3c14e0e22eeb
JCRoooD <juancarlos-99@live.com>
2023-10-13 16:45:28 -0400

    adding integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,13 +3,14 @@
 
 
 def add_integer(a, b=98):
-    """Function that adds two integers"""
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-    if type(a) is not int:
+    if type(a) is not float and type(a) is not int:
         raise TypeError("a must be an integer")
-    if type(b) is not int:
+    if type(b) is not float and type(b) is not int:
         raise TypeError("b must be an integer")
+    if type(a) and type(b) is float:
+        a = int(a)
+        b = int(b)
+    if type(a) and type(b) is int:
+        a = int(a)
+        b = int(b)
     return a + b
