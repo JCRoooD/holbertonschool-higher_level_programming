f3d5599c7e42fbe93b3a6b6545011b520cf8e566
JCRoooD <juancarlos-99@live.com>
2023-10-13 18:10:55 -0400

    fixed typo

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,11 +4,11 @@
 
 def print_square(size):
     """Print a square with the # character"""
-    if type(size) is not int and size is not float < 0:
+    if type(size) is not int and size is not float and size < 0:
         raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError("size must be >=0")
+        raise ValueError("size must be >= 0")
     for i in range(size):
         for j in range(size):
             print("#", end="")
-        print()
+        print("")
