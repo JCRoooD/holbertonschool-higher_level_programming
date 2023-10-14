2432cc541f1d02f75e9df3eea22815fd0ab0386c
JCRoooD <juancarlos-99@live.com>
2023-10-13 20:42:01 -0400

    definition of a rectangle

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for Rectangle class"""
+
+
+class Rectangle:
+    """Defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        """Constructor for the Rectangle class"""
+        self.__width = width
+        self.__height = height
