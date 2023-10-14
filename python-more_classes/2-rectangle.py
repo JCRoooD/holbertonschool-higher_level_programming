ddbc15aba012134a4b3571f05e860eb585ec9051
JCRoooD <juancarlos-99@live.com>
2023-10-13 21:46:38 -0400

    perimeter

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -46,3 +46,14 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
+
+    def area(self):
+        """Return area of Rectangle"""
+        return self.__width * self.__height
+    
+    def perimeter(self):
+        """Return perimeter of Rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2*(self.__width + self.__height)
