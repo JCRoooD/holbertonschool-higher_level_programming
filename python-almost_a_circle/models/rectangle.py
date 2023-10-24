ebb30f29e1d279200dc9bbb3c251413ee0ebfc1d
JCRoooD <juancarlos-99@live.com>
2023-10-24 17:01:21 -0400

    commit fixe error on task 3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,10 +5,6 @@ from models.base import Base
 
 class Rectangle(Base):
     """Rectangle Class"""
-    __width = 0
-    __height = 0
-    __x = 0
-    __y = 0
     
 
     def __init__(self, width, height, x=0, y=0, id=None):
