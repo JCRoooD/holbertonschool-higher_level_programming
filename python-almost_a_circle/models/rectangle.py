bd44ea77ea0a8a04236ef0778ecafaff27c4532e
JCRoooD <juancarlos-99@live.com>
2023-10-24 12:38:24 -0400

    finished task with defining args

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -13,3 +13,8 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """init method rectangle class"""
         super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
