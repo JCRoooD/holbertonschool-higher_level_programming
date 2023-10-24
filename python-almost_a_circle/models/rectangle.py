15418a6c1b6c6f6b0319e5eb0a74d9216b1d1831
JCRoooD <juancarlos-99@live.com>
2023-10-24 14:21:06 -0400

    getters and setters

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,10 +5,6 @@ from models.base import Base
 
 class Rectangle(Base):
     """Rectangle Class"""
-    __width = 0
-    __height = 0
-    __x = 0
-    __y = 0
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """init method rectangle class"""
@@ -17,3 +13,34 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    
+    def width(self):
+        """getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be >=0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be >=0")
+        else:
+            self.__height = value
