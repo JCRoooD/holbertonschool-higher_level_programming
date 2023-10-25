62bc6396e17fb44b001b1787ca3d3c4275fd8def
JCRoooD <juancarlos-99@live.com>
2023-10-25 14:52:33 -0400

    fixed whitespace

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """Rectangle Class"""
-    
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """init method rectangle class"""
         super().__init__(id)
