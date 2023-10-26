f4db960c112ebc9122987eb587351ba559e12b9c
JCRoooD <juancarlos-99@live.com>
2023-10-25 22:19:40 -0400

    class square and constructor module

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""""module for Square class"""
+
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """class Square that inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """string representation"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter for size"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """update attributes"""
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attrs[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """dictionary representation"""
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
