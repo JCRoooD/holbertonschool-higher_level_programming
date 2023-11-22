108e57c38df367b8dd888d0d78ffb7ce8e954dbb
JCRoooD <juancarlos-99@live.com>
2023-11-21 21:08:46 -0400

    left a comma hanging

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- script that creates first_table in hbtn_0c_0
-CREATE TABLE IF NOT EXISTS `first_table` (`id` INT `NAME` VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `NAME` VARCHAR(256));
