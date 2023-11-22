bfeb8889485c000c634dc8739b34e2a17a4d2e56
JCRoooD <juancarlos-99@live.com>
2023-11-21 22:16:47 -0400

    list groups of same score

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- list number of records with same score in second_table
-SELECT score FROM second_table GROUP BY score;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY DESC;
