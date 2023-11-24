20d87d0ea037d60b6898e819ea6a4ce13c845b0e
JCRoooD <juancarlos-99@live.com>
2023-11-24 16:08:48 -0400

    fixed import

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,13 +2,13 @@
 """Module to list all states from database hbtn_0e_0_usa starting with N"""
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     """Function to list all states from database starting with N"""
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE nameLIKEBINARY 'N%' ORDERBYid ASC")
