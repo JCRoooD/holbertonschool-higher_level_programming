5b3aba6c193ff154484b85b7e215676575b6c25a
JCRoooD <juancarlos-99@live.com>
2023-11-25 18:56:15 -0400

    task 9 contains 'a'

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""listing all states from database"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    # Setting up connection to the database
+    user_name = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{user_name}:{password}@localhost:3306/{db_name}"
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name.like('%a%'))
+    for states in state:
+        if state:
+            print(f"{states.id}: {states.name}")
